0744a07b9a053956e67470b9686afc4dfc635975
Sukant Ghosh <sukantghosh@Sukants-MacBook-Air.local>
2025-01-12 21:35:50 +0000

    voting on solana

---
+++ anchor/target/types/voting.ts
@@ -0,0 +1,11 @@
+export type Voting = {
+  "version": "0.0.0",
+  "name": "voting",
+  "instructions": []
+};
+
+export const IDL: Voting = {
+  "version": "0.0.0",
+  "name": "voting",
+  "instructions": []
+};
